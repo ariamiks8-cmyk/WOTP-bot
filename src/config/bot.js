0fe249c23e36a883a31a97dc81d950f68f995bd5
ariamiks8-cmyk <ariamiks8@gmail.com>
2026-09-20 23:33:50 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "WOTP bot",     // this is what people actually see
+        state: "Shibui bot",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
